60db1cbc83cf30ed6f91c2b971b58f4d26b801b0
asenasil <adalberto.sena.silva@everis.com>
2021-04-13 11:17:07 -0300

    Criando a pasta do Projeto Super APP

---
+++ ProjetoFinalSquad3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoFinalSquad3
+//
+//  Created by Adalberto Sena Silva on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjetoFinalSquad3Tests/ProjetoFinalSquad3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ProjetoFinalSquad3Tests.swift
+//  ProjetoFinalSquad3Tests
+//
+//  Created by Adalberto Sena Silva on 13/04/21.
+//
+
+import XCTest
+@testable import ProjetoFinalSquad3
+
+class ProjetoFinalSquad3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
