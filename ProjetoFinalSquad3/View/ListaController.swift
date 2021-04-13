3e29181ed1ef6dad35c9a4558684a1fb9928c520
asenasil <adalberto.sena.silva@everis.com>
2021-04-13 15:17:46 -0300

    Criado a Classe para controlar a Lista de Moedas, criado o xib para customizar a view cell e uma classe para a view cell

---
+++ ProjetoFinalSquad3/View/ListaController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ListaController: UIViewController {
+    
+    // MARK: - Selecao de Outlets
+    
+    
+    
+    
+    
+    // MARK: - Selecao de Atributos da Classe
+    
+    
+    
+    // MARK: - Construtor da Classe
+    
+    
+    
+    // MARK: - Iniciador de Ciclo
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
