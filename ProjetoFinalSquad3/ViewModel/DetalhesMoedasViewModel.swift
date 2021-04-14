7d71718afca45f713aa6c6a468c8678f5cc85e24
asenasil <adalberto.sena.silva@everis.com>
2021-04-14 10:04:56 -0300

    Atualizei o projeto importando os modulos atualizados, fiz uma viewModel para detalhesMoedas criando uma struct.

---
+++ ProjetoFinalSquad3/ViewModel/DetalhesMoedasViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  DetalhesListaCriptomoedas.swift
+//  ProjetoFinalSquad3
+//
+//  Created by Adalberto Sena Silva on 14/04/21.
+//
+
+import Foundation
+
+struct DetalhesMoedasViewModel{
+    
+    let nomeText:String
+    let siglaText:String
+    let cotacaoText: Double
+    let imagemURL: String
+    
+}
