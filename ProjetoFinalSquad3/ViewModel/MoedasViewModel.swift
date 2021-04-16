1620ca143d2bfc94bed674f2b153fcfe74feaafc
asenasil <adalberto.sena.silva@everis.com>
2021-04-16 14:19:51 -0300

    Criamos a strutc MoedaViewModel

---
+++ ProjetoFinalSquad3/ViewModel/MoedasViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  DetalhesListaCriptomoedas.swift
+//  ProjetoFinalSquad3
+//
+//  Created by Adalberto Sena Silva on 14/04/21.
+//
+
+import Foundation
+
+struct MoedasViewModel{
+    
+    let sigla: String
+    let nome: String
+    let valor: Double
+    let imagem: String
+    
+}
