c41f283f7c682da0b6bf40c4539658ac668080ba
asenasil <adalberto.sena.silva@everis.com>
2021-04-16 11:06:15 -0300

    Conseguindo chamar dados da assertID

--- ProjetoFinalSquad3/Controller/MoedasViewController.swift
+++ ProjetoFinalSquad3/Controller/MoedasViewController.swift
@@ -7,39 +7,39 @@
 
 import UIKit
 import Commons
-import CoreData
 import AlamofireImage
+import DetalhesMoedas
 
 
-struct CriptoMoeda: Codable {
-    
-let idIcon, assetID, name: String
-let dataStart, dataEnd, dataQuoteStart, dataQuoteEnd: Int
-let dataOrderbookStart, dataOrderbookEnd, dataTradeStart, dataTradeEnd: Int
-let dataSymbolsCount, volume1HrsUsd, volume1DayUsd, volume1MthUsd: Int
-let priceUsd: Int
+public struct Cripto: Codable {
+//    public let idIcon: String
+    public let assetID: String
+//    public let name: String
+//    public let dataStart, dataEnd, dataQuoteStart, dataQuoteEnd: Int
+//    public let dataOrderbookStart, dataOrderbookEnd, dataTradeStart, dataTradeEnd: Int
+//    public let dataSymbolsCount, volume1HrsUsd, volume1DayUsd, volume1MthUsd: Int
+    public let priceUsd: Int?
 
-enum CodingKeys: String, CodingKey {
-case idIcon = "id_icon"
-case assetID = "asset_id"
-case name
-case dataStart = "data_start"
-case dataEnd = "data_end"
-case dataQuoteStart = "data_quote_start"
-case dataQuoteEnd = "data_quote_end"
-case dataOrderbookStart = "data_orderbook_start"
-case dataOrderbookEnd = "data_orderbook_end"
-case dataTradeStart = "data_trade_start"
-case dataTradeEnd = "data_trade_end"
-case dataSymbolsCount = "data_symbols_count"
-case volume1HrsUsd = "volume_1hrs_usd"
-case volume1DayUsd = "volume_1day_usd"
-case volume1MthUsd = "volume_1mth_usd"
-case priceUsd = "price_usd"
+public enum CodingKeys: String, CodingKey {
+//        case idIcon = "id_icon"
+        case assetID = "asset_id"
+//        case name
+//        case dataStart = "data_start"
+//        case dataEnd = "data_end"
+//        case dataQuoteStart = "data_quote_start"
+////        case dataQuoteEnd = "data_quote_end"
+//        case dataOrderbookStart = "data_orderbook_start"
+//        case dataOrderbookEnd = "data_orderbook_end"
+//        case dataTradeStart = "data_trade_start"
+//        case dataTradeEnd = "data_trade_end"
+//        case dataSymbolsCount = "data_symbols_count"
+//        case volume1HrsUsd = "volume_1hrs_usd"
+//        case volume1DayUsd = "volume_1day_usd"
+//        case volume1MthUsd = "volume_1mth_usd"
+        case priceUsd = "price_usd"
     }
 }
-
-typealias CriptoMoedas = [CriptoMoeda]
+public typealias Criptos = [Cripto]
 
 
 
@@ -57,14 +57,9 @@ class MoedasViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     var favoritos: Favoritos?
     
-    var moedas: CriptoMoedas = []
+//    var moedas: CriptoMoedas = []
 
-    var contexto: NSManagedObjectContext {
-
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        return appDelegate.persistentContainer.viewContext
-
-        }
+  
     
     
     // MARK: - NovoCiclo
@@ -74,38 +69,43 @@ class MoedasViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         self.listaMoedas.delegate = self
         self.listaMoedas.dataSource = self
-       
-        
+        makeRequestDetalhes()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.moedas.count
+        return self.listaDeMoedas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell:CustumTableViewCell? = tableView.dequeueReusableCell(withIdentifier: "CustumTableViewCell", for: indexPath) as? CustumTableViewCell
-        
-        cell?.bitcoinLabel.text 
-        cell?.imagemCriptomoeda.image = UIImage(named: "")
-        cell?.imagemDeFavoritos.image = UIImage(named: "")
-        cell?.siglaLabel.text
-        cell?.cotacaoLabel.text
+
+//        cell?.bitcoinLabel.text
+//        cell?.imagemCriptomoeda.image = UIImage(named: "")
+//        cell?.imagemDeFavoritos.image = UIImage(named: "")
+//        cell?.siglaLabel.text
+//        cell?.cotacaoLabel.text
         
         return cell ?? UITableViewCell()
     }
-    
-    public func makeRequestLista(sigla: String) {
-            let newUrl = ApiRest.MoedaDetalhe
-            let url = URL(string: newUrl)!
+
+     func makeRequestDetalhes() {
+//            let newUrl = ApiRest.MoedaDetalhe.replacingOccurrences(of: "@@@", with: sigla)
+        let url = URL(string: ApiRest.TodasAsMoedas)!
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 print(response as Any)
                 guard let responseData = data else { return }
                 do {
-                    let moedas = try JSONDecoder().decode(Moeda.self, from: responseData)
-                    for moeda in moedas {
+                    let moedas = try JSONDecoder().decode(Criptos.self, from: responseData)
+                    
+                    for moeda in moedas{
                         DispatchQueue.main.async {
-                            self.moedas
+                            self.configuraTela(moeda)
+                            print(moeda)
                         }
                     }
                 } catch let error {
@@ -115,8 +115,12 @@ class MoedasViewController: UIViewController, UITableViewDelegate, UITableViewDa
             task.resume()
         }
     
+    func configuraTela(_ moeda: Cripto){
 
-   
-    
+        let preco = moeda.priceUsd ?? 0
+
+        
+    }
+
 }
 
